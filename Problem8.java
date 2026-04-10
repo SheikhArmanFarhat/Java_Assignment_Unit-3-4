3596516ddce10607beb79b6e81158595deed7c11
SheikhArmanFarhat <armanfarhatsheikh.cse24@sbjit.edu.in>
2026-04-11 03:37:52 +0530

    Add Problem8.java to modify and print a string

---
+++ Problem8.java
@@ -0,0 +1,10 @@
+class Problem8 {
+    public static void main(String[] args) {
+
+        StringBuilder text = new StringBuilder("Hello");
+
+        text.append(" World");
+
+        System.out.println("Modified String: " + text);
+    }
+}
