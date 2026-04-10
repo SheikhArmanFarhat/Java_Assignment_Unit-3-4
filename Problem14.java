6f06d770331337e323130dc9b161bd1130265bd5
SheikhArmanFarhat <armanfarhatsheikh.cse24@sbjit.edu.in>
2026-04-11 03:39:34 +0530

    Implement basic file reading structure with error handling

---
+++ Problem14.java
@@ -0,0 +1,15 @@
+public class Problem14 {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Reading file...");
+        }
+        catch (Exception e) {
+            System.out.println("Error while reading file");
+        }
+        finally {
+            System.out.println("Closing file resources");
+        }
+
+    }
+}
