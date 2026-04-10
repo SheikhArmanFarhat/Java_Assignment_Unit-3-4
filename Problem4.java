a50a9c37c33705f3abc0b6486bfe2b22f306c89c
SheikhArmanFarhat <armanfarhatsheikh.cse24@sbjit.edu.in>
2026-04-11 03:36:04 +0530

    Add Problem4.java to print full name

---
+++ Problem4.java
@@ -0,0 +1,11 @@
+class Problem4 {
+    public static void main(String[] args) {
+
+        String firstName = "Prajwal";
+        String lastName = "Katkar";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println("Full Name: " + fullName);
+    }
+}
