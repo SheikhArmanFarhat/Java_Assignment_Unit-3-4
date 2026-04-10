33a25ede2d000e32996f6eb18acc24039cb8085f
SheikhArmanFarhat <armanfarhatsheikh.cse24@sbjit.edu.in>
2026-04-11 03:34:57 +0530

    Add username cleaning functionality in Problem1

---
+++ Problem1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Problem1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String username = sc.nextLine();
+
+        username = username.trim();        
+        username = username.toLowerCase();
+
+        System.out.println("Cleaned Username: " + username);
+    }
+}
